476941d7670fb70e7153d012b99f5348833995b5
tiexternato <rafaeldosantos__@outlook.com>
2026-06-01 10:25:32 -0300

    Initial commit

---
+++ src/test/java/com/rafasantosdevv/restauranteSpring/RestauranteSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafasantosdevv.restauranteSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestauranteSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
